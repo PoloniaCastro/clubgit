c5ff41f77b0606e06ee4d29273381650ae99990a
Polonia Castro <polonia.castro.vilches@gmail.com>
2019-01-24 12:25:18 -0300

    jjaja
    jajaja

--- ejecutarRegistrarRepartidores.php
+++ ejecutarRegistrarRepartidores.php
@@ -1,10 +1,11 @@
 <?php
 include_once 'clases/servicio.php';
 include_once 'clases/conexion.php';
-
+include_once("menu.php");
 $nombreRepartidor = $_POST["txtNombreRepartidores"];
+$rpId=  $_SESSION["id2"];
 
-$consulta = "INSERT INTO repartidores(nombre_repartidor) VALUES ('".$nombreRepartidor."')";
+$consulta = "INSERT INTO repartidores(nombre_repartidor, id_rp) VALUES ('".$nombreRepartidor."', '".$rpId."' )";
 $resultado = mysqli_query( $conexion, $consulta ) or die ( "Algo ha ido mal en la consulta a la base de datos");
 
 if($resultado)
