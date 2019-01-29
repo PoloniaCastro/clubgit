7491378f3f420cbe6f84feffbbae374fab99daf7
Polonia Castro <polonia.castro.vilches@gmail.com>
2019-01-29 18:08:59 -0300

    sdfsdf
    fsdfds

--- ejecutarExportarAsistentes.php
+++ ejecutarExportarAsistentes.php
@@ -12,9 +12,7 @@ $selectFiesDes = $_POST["selectFiestaDestino"];
 
 $consultaSelect = "SELECT * FROM asistencia where fiesta = '".$selectFiesta."' and estado ='".$selectEstado."'";
 $resultadoSelect = mysqli_query( $conexion, $consultaSelect ) or die ( "Algo ha ido mal en la consulta a la base de datos");
-$fila = mysqli_fetch_array($resultadoSelect);
-$scan = $fila[0];
-while($scan !=0)
+while($fila = mysqli_fetch_array($resultadoSelect))
 
 {
   $consulta = "INSERT INTO `asistencia`(id_asistencia, nombre, rut, rp, repartidor, estado, fiesta) VALUES ('','nombre','rut','rp','repartidor','estado','".$selectFiesDes."')";
